a4cbf307553b206f243429ceb085ef5169995cf6
Daniel Martínez Rosillo <daniel.martinez@abamobile.com>
2023-06-07 14:06:03 +0200

    Initial Commit

---
+++ Rick&MortyApp/Rick_MortyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rick_MortyAppApp.swift
+//  Rick&MortyApp
+//
+//  Created by ABAMobile-Dani on 7/6/23.
+//
+
+import SwiftUI
+
+@main
+struct Rick_MortyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
